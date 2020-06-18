5a5d44cd5d537a7512263ba94401d0e7ca4c6cf2
Oswald Quevillart <o.quevillart@codeur.online>
2020-06-18 09:46:05 +0200

    README + bords infranchissable

--- script.js
+++ script.js
@@ -9,16 +9,29 @@ document.onkeydown = function(event){
   switch(keyCode){
     case 38:
       y = y - 40; // ou y-=40;
+      if (y < 0) {
+        y = 0;
+      }
       break;
     case 39:
       x = x + 40;
+      if (x > 760) {
+      x = 760;
+      }
       break;
     case 40:
       y = y + 40;
+      if (y > 760) {
+        y = 760;
+      }
       break;
     case 37:
       x = x - 40;
+      if (x < 0) {
+        x = 0;
+      }
       break;
+
   }
   stylePion.left = String(x) + 'px';
   stylePion.top = String(y) + 'px';

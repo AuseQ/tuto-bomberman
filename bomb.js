86300a191e6cc3cbae58cc7fb3402c2f3f37721f
S-Thibault <sylvain.thibault@infamail.com>
2020-07-02 16:59:36 +0200

    Update bomb.js

--- bomb.js
+++ bomb.js
@@ -32,10 +32,32 @@ function createBombe(blockGrid) {
       document.getElementById('plateau').appendChild(bombe);
 
       setTimeout(function explose() {
+        if (bombe.x < H_GRID - 1) {
+
+          blockGrid[bombe.x + 1][bombe.y].traverser = true;
+          blockGrid[bombe.x + 1][bombe.y].style.backgroundColor = "green";
+        }
+        if (bombe.x > 0) {
+
+          blockGrid[bombe.x - 1][bombe.y].traverser = true;
+          blockGrid[bombe.x - 1][bombe.y].style.backgroundColor = "green";
+        }
+        if (bombe.y < V_GRID - 1) {
+
+          blockGrid[bombe.x][bombe.y + 1].traverser = true;
+          blockGrid[bombe.x][bombe.y + 1].style.backgroundColor = "green";
+        }
+        if (bombe.y > 0) {
+
+          blockGrid[bombe.x][bombe.y - 1].traverser = true;
+          blockGrid[bombe.x][bombe.y - 1].style.backgroundColor = "green";
+        }
+
         bombe.remove();
         blockGrid[bombe.x][bombe.y].traverser = true;
         blockGrid[bombe.x][bombe.y].bombe = false;
         bombe.libre = true;
+        blockGrid[bombe.x][bombe.y].style.backgroundColor = "green";
       }, 2000);
 
     }
